4184eee6830bcd751642466b2004294051507826
Book2020 <Book2020@DESKTOP-UQ25FCO>
2021-11-08 15:11:06 -0300

    Adicionar arquivos de projeto.

---
+++ Clientes/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Clientes
+{
+    public class Cliente
+    {
+        public string nome;
+        public double cpf;
+        public double cep;
+        public string pet;
+        public double telefone;
+        public string email;
+
+        public void cadastrarCliente()
+        {
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite o CPF");
+            cpf = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o CEP");
+            cep = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do seu Pet");
+            pet = Console.ReadLine();
+            Console.WriteLine("Digite o Telefone");
+            telefone = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o email");
+            email = Console.ReadLine();
+
+
+        }
+    }
+}
